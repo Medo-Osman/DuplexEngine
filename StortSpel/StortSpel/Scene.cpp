565a9df7d4278a7ff310bb38339b9c0c962a1839
Oskar <oskar_ozkizz_wow@hotmail.com>
2020-11-26 15:19:01 +0100

    Removed trianglemeshes on undesierable physicscomponents.

--- StortSpel/StortSpel/Scene.cpp
+++ StortSpel/StortSpel/Scene.cpp
@@ -216,7 +216,7 @@ void Scene::addCheckpoint(const Vector3& position)
 
 	addComponent(checkPoint, "sound", new AudioComponent(L"OnPickup.wav", false, 0.1f));
 
-	createNewPhysicsComponent(checkPoint, false, "", PxGeometryType::eTRIANGLEMESH);
+	createNewPhysicsComponent(checkPoint, false);
 }
 
 void Scene::addBarrelDrop(Vector3 Position)
@@ -903,7 +903,7 @@ void Scene::loadTestLevel(Scene* sceneObject, bool* finished)
 		clownMask->setPosition(Vector3(-11.5f, 60.f, 290.f));
 		clownMask->setRotation(XMConvertToRadians(7.f), XMConvertToRadians(180.f), XMConvertToRadians(0.f));
 
-		sceneObject->createNewPhysicsComponent(clownMask, false, "", PxGeometryType::eTRIANGLEMESH);
+		sceneObject->createNewPhysicsComponent(clownMask, false);
 	}
 	Entity* goalTrigger = sceneObject->addEntity("trigger");
 	if (goalTrigger)
